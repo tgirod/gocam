e1004543dd0b6744483b0cbc4386d76d06604857
Tomtom <tomtom@herbesfolles.org>
2018-01-27 12:57:39 +0100

    added Tangents function

--- core/offset.go
+++ core/offset.go
@@ -2,56 +2,44 @@ package core
 
 // Offsetter defines the necessary interface to compute an offset
 type Offsetter interface {
-	TanStart() Vector
-	TanEnd() Vector
+	Tangents() (Vector, Vector)
 }
 
-// TanStart returns the tangent vector at the starting point of line l
-func (l *Line) TanStart() Vector {
-	return l.Vector().Unit()
-}
-
-// TanEnd returns the tangent vector at the ending point of line l
-func (l *Line) TanEnd() Vector {
-	return l.Vector().Unit()
+// Tangents returns the tangents of Line l at starting and ending points
+func (l *Line) Tangents() (Vector, Vector) {
+	t := l.Vector().Unit()
+	return t, t
 }
 
 // TanStart returns the tangent vector at the starting point of arc a
-func (a *Arc) TanStart() Vector {
-	vec := a.Start.Sub(a.Center).Normal()
+func (a *Arc) Tangents() (Vector, Vector) {
+	tanStart := a.Start.Sub(a.Center).Normal()
+	tanEnd := a.End.Sub(a.Center).Normal()
 	if !a.CW {
-		vec = vec.Neg()
+		tanStart = tanStart.Neg()
+		tanEnd = tanEnd.Neg()
 	}
-	return vec
-}
-
-// TanEnd returns the tangent vector at the ending point of arc a
-func (a *Arc) TanEnd() Vector {
-	vec := a.End.Sub(a.Center).Normal()
-	if !a.CW {
-		vec = vec.Neg()
-	}
-	return vec
+	return tanStart, tanEnd
 }
 
 // Offset method returns a new line at distance d of line l
 func (l *Line) Offset(d float64) *Line {
-	var nl Line
-	// straight line
-	off := l.TanStart().Normal().Mul(d)
-	nl.Start = l.Start.Add(off)
-	nl.End = l.End.Add(off)
-	return &nl
+	t, _ := l.Tangents()
+	off := t.Normal().Mul(d)
+	return &Line{
+		Start: l.Start.Add(off),
+		End:   l.End.Add(off)}
 }
 
 // Offset method returns a new arc at distance d of arc a
 func (a *Arc) Offset(d float64) *Arc {
-	radius := a.Radius()
-	ratio := (radius + d) / radius
+	ts, te := a.Tangents()
+	offStart := ts.Normal().Mul(d)
+	offEnd := te.Normal().Mul(d)
 
 	return &Arc{
-		Start: a.Start.Sub(a.Center).Mul(ratio).Add(a.Center),
-		End:   a.End.Sub(a.Center).Mul(ratio).Add(a.Center),
+		Start: a.Start.Add(offStart),
+		End:   a.End.Add(offEnd),
 		CW:    a.CW}
 }
 
