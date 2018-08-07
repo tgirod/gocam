fce91961b8ec01473ae73d0754a4a22b9a183276
Thomas Girod <thomas.girod@loria.fr>
2018-08-07 09:59:32 +0200

    removing debug logs

--- model.go
+++ model.go
@@ -8,18 +8,15 @@ type Model []Path
 
 // Append Move to the model by concatenating it to an existing one if possible.
 func (m *Model) Append(mo Move) {
-	Log.Printf("Appending: %v\n", mo)
 	for i := 0; i < len(*m); i++ {
 		p := &(*m)[i]
 		if ok := p.Append(mo); ok {
-			Log.Printf("updated path: %v", p)
 			return
 		}
 	}
 	p := &Path{}
 	p.Append(mo)
 	*m = append(*m, *p)
-	Log.Printf("new path: %v", *p)
 }
 
 func (m *Model) Merge() {
